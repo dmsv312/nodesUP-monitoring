5cea30760c559876707a92ae556e5db9b09a0fa5
А.Хомченко <a.khomchenko@nsu.ru>
2022-11-07 15:50:22 +0700

    USG-5492 Routing

---
+++ www/frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link>
+    </div>
+    Home page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
