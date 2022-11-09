b6f9362f94f4d38eb3f72ae431bb2978c37f73ea
А.Хомченко <a.khomchenko@nsu.ru>
2022-11-09 16:46:09 +0700

    USG-5462 Login page

--- www/frontend/src/views/Login.vue
+++ www/frontend/src/views/Login.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="login">
-    Login page
+    <div class="logo-wrapper">
+      <img src="../assets/images/logo-white.svg" class="logo">
+    </div>
+    <div class="content-wrapper">
+      <div class="content">
+        <h1>Личный кабинет абонента</h1>
+        <p>Чтобы войти в личный кабинет, введите номер договора и пароль.</p>
+        <form action="" method="post">
+          <div class="form-group">
+            <label for="username">Логин</label>
+            <input type="text" class="w-100" v-model="username" id="username" placeholder="Введите логин">
+          </div>
+          <div class="form-group">
+            <label for="password">Пароль</label>
+            <input type="password" class="w-100" v-model="password" id="password" placeholder="Введите пароль">
+          </div>
+          <div class="form-buttons">
+            <button class="btn btn-primary">Войти</button>
+          </div>
+          <div class="forgot-password">
+            Забыли пароль? <router-link to="/">Восстановить</router-link>
+          </div>
+
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  props: {
+    username: String,
+    password: String
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style src="../assets/css/style.css" />
+<style scoped src="../assets/css/login.css" />
+<style scoped src="../assets/css/input-text.css" />
