16fe48adac4e0ef544b99dbf1f853c7a916cf6fb
А.Хомченко <a.khomchenko@nsu.ru>
2022-11-14 17:04:00 +0700

    header scss

--- www/frontend/src/components/header/HeaderContent.vue
+++ www/frontend/src/components/header/HeaderContent.vue
@@ -30,24 +30,22 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   header {
     height: 105px;
     background: #337AB7;
     box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
     color: #ffffff;
-  }
-
-  header a {
-    color: #ffffff;
-  }
-
-  header .content {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    flex-wrap: nowrap;
+    .content {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      flex-wrap: nowrap;
+    }
+    a {
+      color: #ffffff;
+    }
   }
   .logo-link {
     margin-right: 25px;
@@ -62,38 +60,38 @@ export default {
   .language-choice {
     margin-left: 33px;
     width: 40px;
-  }
-  .language-choice .language {
-    font-weight: 700;
-    display: flex;
-    flex-wrap: nowrap;
-    align-items: center;
-    justify-content: space-between;
-  }
-  .language-choice .language:after {
-    display: block;
-    content: '';
-    width: 10px;
-    height: 10px;
-    background: url("../../assets/images/arrow-white.svg") no-repeat center center;
-    cursor: pointer;
-    transform: rotate(90deg);
-  }
-  .language-choice .languages {
-    position: absolute;
-    border: 1px solid #fff;
-    width: 40px;
-    background: #FFFFFF;
-    box-shadow: 0 6px 58px rgba(49, 49, 49, 0.4);
-    border-radius: 17px;
-    margin-left: -12px;
-    padding: 5px 0;
-  }
-  .language-choice .languages a {
-    display: block;
-    color: #337AB7;
-    text-align: center;
-    font-weight: 700;
+    .language {
+      font-weight: 700;
+      display: flex;
+      flex-wrap: nowrap;
+      align-items: center;
+      justify-content: space-between;
+      &:after {
+        display: block;
+        content: '';
+        width: 10px;
+        height: 10px;
+        background: url("../../assets/images/arrow-white.svg") no-repeat center center;
+        cursor: pointer;
+        transform: rotate(90deg);
+      }
+    }
+    .languages {
+      position: absolute;
+      border: 1px solid #fff;
+      width: 40px;
+      background: #FFFFFF;
+      box-shadow: 0 6px 58px rgba(49, 49, 49, 0.4);
+      border-radius: 17px;
+      margin-left: -12px;
+      padding: 5px 0;
+      a {
+        display: block;
+        color: #337AB7;
+        text-align: center;
+        font-weight: 700;
+      }
+    }
   }
   .user-link {
     margin-left: 24px;
@@ -103,17 +101,17 @@ export default {
     justify-content: space-between;
     font-weight: 700;
     text-decoration: none;
-  }
-  .user-link:after {
-    display: block;
-    content: '';
-    width: 10px;
-    height: 10px;
-    background: url("../../assets/images/arrow-white.svg") no-repeat center center;
-    cursor: pointer;
-    margin-left: 10px;
-  }
-  .user-link:focus, .user-link:hover {
-    text-decoration: underline;
+    &:after {
+      display: block;
+      content: '';
+      width: 10px;
+      height: 10px;
+      background: url("../../assets/images/arrow-white.svg") no-repeat center center;
+      cursor: pointer;
+      margin-left: 10px;
+    }
+    &:focus, &:hover {
+      text-decoration: underline;
+    }
   }
 </style>
