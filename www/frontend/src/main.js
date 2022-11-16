a80f20df383a25e383aeccbaf12dce1fb498936a
А.Хомченко <a.khomchenko@nsu.ru>
2022-11-16 10:31:38 +0700

    el colors

--- www/frontend/src/main.js
+++ www/frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
+import './assets/styles/_colors.scss' // цветовая схема
 import './assets/styles.scss' // общие стили
 import 'element-plus/dist/index.css' // element-plus
 import './assets/styles/element-plus.scss' // custom element-plus
