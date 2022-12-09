927c150346c02fc4b0bc99b84cea516787ad1d39
А.Хомченко <a.khomchenko@nsu.ru>
2022-12-09 10:45:23 +0700

    Run in Docker Desktop for Windows

--- www/frontend/vue.config.js
+++ www/frontend/vue.config.js
@@ -1,12 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
-  // css: {
-  //   loaderOptions: {   // так
-  //   additionalData: {  // или так
-  //     sass: {
-  //       prependData: ' @import "@/assets/_variables.scss"; // автоматически подключается во все файлы
-  //     }
-  //   }
-  // }
 })
